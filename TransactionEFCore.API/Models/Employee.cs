bf7cd2d9bbc24965faa09faf6eee9bbd471812e4
David <david_horeb@hotmail.com>
2021-05-04 07:48:58 -0300

    Add Models

---
+++ TransactionEFCore.API/Models/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TransactionEFCore.API.Models
+{
+    public class Employee
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Empleado")]
+        [MaxLength(50, ErrorMessage = "El {0} no puede tener mas de {1} caracteres.")]
+        public string Name { get; set; }
+
+        [Display(Name = "Departamento")]
+        [MaxLength(50, ErrorMessage = "El {0} no puede tener mas de {1} caracteres.")]
+        public string Departament { get; set; }
+
+        [Display(Name = "Inicio de Actividad")]
+        [MaxLength(50, ErrorMessage = "El {0} no puede tener mas de {1} caracteres.")]
+        public string JoinDate { get; set; }
+    }
+}
---
+++ TransactionEFCore.API/Models/Salary.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TransactionEFCore.API.Models
+{
+    public class Salary
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int IdEmp { get; set; }
+
+        [Display(Name = "Salario Mensual")]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal GrossSalary { get; set; }
+    }
+}
